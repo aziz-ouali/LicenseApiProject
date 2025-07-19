d31c939602a5ff2dcb34380037fab8a9981c4774
AZIZ OUALI <48460519+aziz-ouali@users.noreply.github.com>
2025-07-19 23:55:26 +0100

    Data

---
+++ AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using LicenseApiProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LicenseApiProject.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Device> Devices => Set<Device>();
+        public DbSet<License> Licenses => Set<License>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+
+            modelBuilder.Entity<Device>()
+                .HasIndex(d => d.DeviceIdentifier)
+                .IsUnique();
+        }
+    }
+}
