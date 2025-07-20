673daa3dab72a4715a17426f3506345fe7cb9421
AZIZ OUALI <48460519+aziz-ouali@users.noreply.github.com>
2025-07-20 02:17:06 +0100

    AppDbContext.cs

--- AppDbContext.cs
+++ AppDbContext.cs
@@ -12,14 +12,26 @@ namespace LicenseApiProject.Data
         public DbSet<License> Licenses => Set<License>();
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<User>()
-                .HasIndex(u => u.Username)
-                .IsUnique();
+{
+    modelBuilder.Entity<User>()
+        .HasIndex(u => u.Username)
+        .IsUnique();
 
-            modelBuilder.Entity<Device>()
-                .HasIndex(d => d.DeviceIdentifier)
-                .IsUnique();
-        }
+    modelBuilder.Entity<Device>()
+        .HasIndex(d => d.DeviceIdentifier)
+        .IsUnique();
+
+    modelBuilder.Entity<License>()
+        .HasOne(l => l.User)
+        .WithMany(u => u.Licenses)
+        .HasForeignKey(l => l.UserID)
+        .OnDelete(DeleteBehavior.Cascade);
+
+    modelBuilder.Entity<License>()
+        .HasOne(l => l.Device)
+        .WithMany(d => d.Licenses)
+        .HasForeignKey(l => l.DeviceID)
+        .OnDelete(DeleteBehavior.Cascade);
+}
     }
 }
